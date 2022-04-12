5975619a0f96834d1c01b2d9677d8ecddead92b3
Joshua D. Miller <josh.miller@outlook.com>
2022-04-12 16:49:21 -0400

    Update Expiration Only.sh

--- jamf Extension Attributes/Expiration Only.sh
+++ jamf Extension Attributes/Expiration Only.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 : '
--------------------------
-| macOSLAPS EA Password |
--------------------------
-| Captures the Password from the file outputted
+---------------------------
+| macOSLAPS EA Expiration |
+---------------------------
+| Captures the Expiration from the file outputted
 | to the filesystem and sends the result to jamf
 | in the following format:
-|     | Password: PasswordHere |
+|     | Expiration: Expiration Date |
 ------------------------------------------------------------
 | Created: Richard Purves - https://github.com/franton
 | Last Update By: Joshua D. Miller - josh.miller@outlook.com
@@ -22,15 +22,15 @@ if [ -e $LAPS ] ; then
     # Ask macOSLAPS to write out the current password and echo it for the Jamf EA
     $LAPS -getPassword
     CURRENT_EXPIRATION=$(/bin/cat "$EXP_FILE")
-    # Test $current_password to ensure there is a value
+    # Test $CURRENT_EXPIRATION to ensure there is a value
     if [ -z "$CURRENT_EXPIRATION" ]; then
         # Write no expiration date is present and send to
         # jamf Pro
         /bin/echo "<result>No Expiration Date Present</result>"
         exit 0
     else
-        /bin/echo "<result>| Password: $CURRENT_EXPIRATION |</result>"
-        # Run macOSLAPS a second time to remove the password file
+        /bin/echo "<result>| Expiration: $CURRENT_EXPIRATION |</result>"
+        # Run macOSLAPS a second time to remove the Expiration file
         # and expiration date file from the system
         $LAPS
     fi
